0158b831c4286ba70b8dd6710e5cec6bb613fefd
Omega515 <ruslan515yulaev@yandex.ru>
2022-08-19 08:50:24 +0500

    step_02. task_A

---
+++ Z_function/step_02/task_A.java
@@ -0,0 +1,105 @@
+package z_func.step_02;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+
+public class task_A {
+    /*
+    Нашел данный класс для ускорения ввода и вывода
+     */
+    private static class Parser {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Parser(InputStream in) {
+            din = new DataInputStream(in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead =  0;
+        }
+        public String nextString(int maxSize) {
+            byte[] ch = new byte[maxSize];
+            int point =  0;
+            try {
+                byte c = read();
+                while (c == ' ' || c == '\n' || c=='\r')
+                    c = read();
+                while (c != ' ' && c != '\n' && c!='\r') {
+                    ch[point++] = c;
+                    c = read();
+                }
+            } catch (Exception e) {}
+            return new String(ch, 0,point);
+        }
+        public int nextInt() {
+            int ret =  0;
+            boolean neg;
+            try {
+                byte c = read();
+                while (c <= ' ')
+                    c = read();
+                neg = c == '-';
+                if (neg)
+                    c = read();
+                do {
+                    ret = ret * 10 + c - '0';
+                    c = read();
+                } while (c > ' ');
+
+                if (neg) return -ret;
+            } catch (Exception e) {}
+            return ret;
+        }
+        public long nextLong() {
+            long ret =  0;
+            boolean neg;
+            try {
+                byte c = read();
+                while (c <= ' ')
+                    c = read();
+                neg = c == '-';
+                if (neg)
+                    c = read();
+                do {
+                    ret = ret * 10 + c - '0';
+                    c = read();
+                } while (c > ' ');
+
+                if (neg) return -ret;
+            } catch (Exception e) {}
+            return ret;
+        }
+        private void fillBuffer() {
+            try {
+                bytesRead = din.read(buffer, bufferPointer =  0, BUFFER_SIZE);
+            } catch (Exception e) {}
+            if (bytesRead == -1) buffer[ 0] = -1;
+        }
+
+        private byte read() {
+            if (bufferPointer == bytesRead) fillBuffer();
+            return buffer[bufferPointer++];
+        }
+    }
+
+    public static void main(String[] args) {
+        Parser sc = new Parser(System.in);
+        String s = sc.nextString(3000);
+        int n = s.length();
+//        int[] z = new int[n];
+//        z[0] = 0;
+//        System.out.print(z[0]);
+        System.out.print(0);
+        System.out.print(" ");
+        for (int i = 1; i < n; i++) {
+            int j = 0;
+            while ((i + j) < n && s.charAt(j) == s.charAt(i + j))
+                j++;
+//            z[i] = j;
+            System.out.print(j);
+            System.out.print(" ");
+        }
+    }
+
+}
